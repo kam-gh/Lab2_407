b9de60ff8aef9e72ce1a36a1963da37579217f88
kam-gh <kdomagala@wisc.edu>
2023-09-25 01:22:27 -0500

    Calculator progress pt.3

--- app/src/main/java/com/cs407/calculatorapp/MainActivity.java
+++ app/src/main/java/com/cs407/calculatorapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -27,17 +28,19 @@ public class MainActivity extends AppCompatActivity {
             intValue1 = Integer.parseInt(text1);
         } catch (Exception e) {
             goToActivity("Error: Must enter two integers");
+            return;
         }
+
 
         EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
         String text2 = myTextField2.getText().toString();
 
         try {
             intValue2 = Integer.parseInt(text2);
-        } catch(NumberFormatException e) {
-            goToActivity("Error: Must enter two integers.");
+        } catch (Exception e) {
+            goToActivity("Error: Must enter two integers");
+            return;
         }
-
 
         int finalVal = intValue1 + intValue2;
         String finalStr = String.valueOf(finalVal);
@@ -51,9 +54,23 @@ public class MainActivity extends AppCompatActivity {
         String text1 = myTextField1.getText().toString();
         int intValue1 = Integer.parseInt(text1);
 
+        try {
+            intValue1 = Integer.parseInt(text1);
+        } catch (Exception e) {
+            goToActivity("Error: Must enter two integers");
+            return;
+        }
+
         EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
         String text2 = myTextField2.getText().toString();
         int intValue2 = Integer.parseInt(text2);
+
+        try {
+            intValue2 = Integer.parseInt(text2);
+        } catch (Exception e) {
+            goToActivity("Error: Must enter two integers");
+            return;
+        }
 
         int finalVal = intValue1 - intValue2;
         String finalStr = String.valueOf(finalVal);
@@ -67,9 +84,23 @@ public class MainActivity extends AppCompatActivity {
         String text1 = myTextField1.getText().toString();
         int intValue1 = Integer.parseInt(text1);
 
+        try {
+            intValue1 = Integer.parseInt(text1);
+        } catch (Exception e) {
+            goToActivity("Error: Must enter two integers");
+            return;
+        }
+
         EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
         String text2 = myTextField2.getText().toString();
         int intValue2 = Integer.parseInt(text2);
+
+        try {
+            intValue2 = Integer.parseInt(text2);
+        } catch (Exception e) {
+            goToActivity("Error: Must enter two integers");
+            return;
+        }
 
         int finalVal = intValue1 * intValue2;
         String finalStr = String.valueOf(finalVal);
@@ -83,11 +114,25 @@ public class MainActivity extends AppCompatActivity {
         String text1 = myTextField1.getText().toString();
         int intValue1 = Integer.parseInt(text1);
 
+        try {
+            intValue1 = Integer.parseInt(text1);
+        } catch (Exception e) {
+            goToActivity("Error: Must enter two integers");
+            return;
+        }
+
         EditText myTextField2 = (EditText) findViewById(R.id.myTextField2);
         String text2 = myTextField2.getText().toString();
         int intValue2 = Integer.parseInt(text2);
         if (intValue2 == 0) {
             goToActivity("Error: Divide by 0.");
+        }
+
+        try {
+            intValue2 = Integer.parseInt(text2);
+        } catch (Exception e) {
+            goToActivity("Error: Must enter two integers");
+            return;
         }
 
         int finalVal = intValue1 / intValue2;
